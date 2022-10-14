0e04fde83650ed05aae80ddb05550f7b53d07bcc
Henrique Moises <hmoises@fortinet-us.com>
2022-10-14 19:09:32 -0300

    initial release

--- main.tf
+++ main.tf
@@ -1,13 +1,24 @@
 terraform {
+  required_version = ">= 0.12.24"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
+        source = "hashicorp/aws"
+        version = "4.34.0"
     }
   }
 }
 
-# Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+    region  = var.region
 }
+
+provider "aws" {
+    alias   = "AWS-Account-0"
+    region  = var.region
+}
+
+provider "aws" {
+    alias   = "AWS-Account-1"
+    region  = var.region
+}
+
