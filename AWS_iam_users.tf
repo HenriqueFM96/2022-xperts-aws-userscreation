dbd54710fe9da50d886659824e686d33d04f1247
Henrique Moises <hmoises@fortinet-us.com>
2022-10-14 20:42:30 -0300

    initial release

--- AWS_iam_users.tf
+++ AWS_iam_users.tf
@@ -9,3 +9,16 @@ resource "aws_iam_user_login_profile" "stdts_login" {
     password_length = 10 
     password_reset_required = true
 }
+
+resource "aws_identitystore_user" "students-users" {
+  identity_store_id = tolist(data.aws_ssoadmin_instances.example.identity_store_ids)[0]
+
+  count = 5
+  display_name = "user-student-${count.index}"
+  user_name    = "user-student-${count.index}"
+
+  name {
+    given_name  = "student-${count.index}"
+    family_name = "user-${count.index}"
+  }
+}
