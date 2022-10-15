9f5c5ee7b7b1c531cd16b87e82fea797defb481e
Henrique Moises <hmoises@fortinet-us.com>
2022-10-14 21:19:28 -0300

    initial release

--- AWS_iam_Accounts.tf
+++ AWS_iam_Accounts.tf
@@ -3,3 +3,12 @@ resource "aws_iam_account_alias" "Account-00112233" {
   account_alias = file(account_alias.csv)
 }
 */
+
+resource "aws_account_alternate_contact" "operations" {
+  alternate_contact_type = "Operations"
+  name          = "Example"
+  title         = "Example"
+  email_address = "test@example.com"
+  phone_number  = "+1234567890"
+  account_id = "579722002509"
+}
